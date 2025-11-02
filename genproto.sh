fac36c59e680d696172016e090de456f2361a262
dresswithpockets <dresswithpockets@pm.me>
2025-11-02 14:47:21 -0700

    add missing go types in proto

--- genproto.sh
+++ genproto.sh
@@ -8,4 +8,6 @@ protoc \
   --go_opt=Msteammessages_auth.steamclient.proto=./steam \
   --go_opt=Menums.proto=./steam \
   protobufs/steam/steammessages_auth.steamclient.proto \
+  protobufs/steam/steammessages_unified_base.steamclient.proto \
+  protobufs/steam/steammessages_base.proto \
   protobufs/steam/enums.proto
